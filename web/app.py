8bdc73090059045a0f0ac3e35b8c697c885144f2
Todd Densmore <469399+tdensmore@users.noreply.github.com>
2022-07-29 14:06:48 -0400

    Adding connection port for Redis

--- web/app.py
+++ web/app.py
@@ -3,7 +3,7 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis-lb', port=6379)
+cache = redis.Redis(host='database-redis', port=6379)
 
 
 def get_hit_count():
